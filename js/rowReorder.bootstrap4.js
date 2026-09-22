73f2518717999a52904627c90ae1abedbe6432ef
dtbuild <dtbuild@rigil>
2026-09-22 10:52:11 +0000

    0f1c09ead57fb5ff0b34e4285510ab69a6ce3cd8 Release 2.1.0
    Sync to source repo @0f1c09ead57fb5ff0b34e4285510ab69a6ce3cd8

--- js/rowReorder.bootstrap4.js
+++ js/rowReorder.bootstrap4.js
@@ -1,4 +1,4 @@
-/*! RowReorder Bootstrap 4 styling 2.0.0 for DataTables
+/*! RowReorder Bootstrap 4 styling 2.1.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
--- js/rowReorder.bootstrap4.min.mjs
+++ js/rowReorder.bootstrap4.min.mjs
@@ -1,4 +1,4 @@
-/*! RowReorder Bootstrap 4 styling 2.0.0 for DataTables
+/*! RowReorder Bootstrap 4 styling 2.1.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-bs4";import RowReorder from"datatables.net-rowreorder";export default DataTable;
